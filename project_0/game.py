f5043b51777aad1b60ca927891a1ad1861b81bf3
mureich81 <ziraffa81@gmail.com>
2022-02-10 22:36:43 +0300

    modified

--- project_0/game.py
+++ project_0/game.py
@@ -3,7 +3,7 @@ number = np.random.randint(1, 100) # guess number
 count = 0
 while True:
     count += 1
-    predict_number = int(input('Guess number you looser:'))
+    predict_number = int(input('Your number:'))
     if predict_number < number:
         print('You must input a larger number')
     elif predict_number > number:
